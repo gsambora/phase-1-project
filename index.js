bf633d750d79af35d8ad9c1261f7a2b4310e171b
Hana <grace.sambora@gmail.com>
2023-06-22 19:07:16 -0500

    Cleaned up and added comments

--- index.js
+++ index.js
@@ -1,85 +1,4 @@
-function changeSong(name, picture, audio) {
-    //Updates "Now Playing:" title, album cover, and audio player
-    let player = document.querySelector(".musicPlayer");
-
-    //Title
-    player.childNodes[1].innerHTML = "Now Playing: " + name
-    //Album cover
-    player.childNodes[3].src = picture
-    player.childNodes[3].alt = "K.K. Slider's "+ name +" Album Cover"
-    //Audio player
-    player.childNodes[5].src = audio
-    player.childNodes[5].childNodes[1].href = audio
-}
-
-function addVillager(villager, island=true){
-    //Adds villager cards either to current island or search results
-    console.log(villager)
-    const newVill = document.createElement("div");
-    newVill.classList.add("card");
-    
-    const villName = document.createElement("h2");
-    villName.innerHTML = Object.values(villager.name)[0];
-    
-    const villIcon = document.createElement("img");
-    villIcon.src = villager.icon_uri;
-    villIcon.alt = "Icon of "+ villName.innerHTML;
-    villIcon.classList.add("villager-icon")
-    villIcon.hidden = true;
-
-    const villPic = document.createElement("img");
-    villPic.src = villager.image_uri;
-    villPic.alt = "Picture of "+ villName.innerHTML;
-    villPic.classList.add("villager-pic")
-
-    const villSpePers = document.createElement("h4");
-    villSpePers.innerHTML = villager.personality + " " + villager.species
-
-    //newVill.id="card-"+villName.innerHTML;
-
-    if(island === true){
-        document.querySelector("#current-island").append(newVil);
-    } else{
-        const villAdd = document.createElement("button");
-        villAdd.id = "search_"+villName.innerHTML
-        villAdd.innerHTML = "Add to island"
-        
-        newVill.append(villName, villIcon, villPic, villSpePers, villAdd)
-
-        document.querySelector("#vill-results").append(newVill);
-        villBtns(villAdd, newVill, false)
-    }
-
-}
-
-function villBtns(button, villElement, island=true, wanted=true){
-    button.addEventListener("click", ()=>{
-        console.log(villElement)
-        if(island === true){
-            if(wanted === true){
-                console.log("want to remove")
-                villElement.querySelector(".villager-pic").hidden = true;
-                villElement.querySelector(".villager-icon").hidden = false;
-                villElement.querySelector("button").innerHTML = "Nevermind, I want them"
-                wanted = false
-            } else{
-                console.log("want them back")
-                villElement.querySelector(".villager-pic").hidden = false;
-                villElement.querySelector(".villager-icon").hidden = true;
-                villElement.querySelector("button").innerHTML = "Planning to remove"
-                wanted = true
-            }
-            
-        } else{
-            console.log("adding to island");
-            villElement.querySelector("button").innerHTML = "Planning to remove"
-            document.querySelector("#current-island").append(villElement);
-            island = true
-
-        }
-    })
-}
-
+//Main function is called after DOM content is loaded
 document.addEventListener("DOMContentLoaded", function() {
     //MUSIC PLAYER
     const inputMusic = document.querySelectorAll("form")[0];
@@ -113,33 +32,123 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     })
     
-    //Villager search by name
+    //VILLAGER SEARCH - NAME
     const villForm = document.querySelectorAll("form")[1];
     
     villForm.addEventListener("submit", (event)=>{
         event.preventDefault();
+        document.querySelector("#vill-results").innerHTML = '';
         const inputVill = document.querySelector("input#villSearch")
         
         fetch('https://acnhapi.com/v1/villagers')
         .then((response)=>response.json())
         .then((data)=>{
+            //Convert collection of all villagers to an array
             const allVillagers = Object.values(data);
-            let newVillager = Object;
-
+            
+            //Iterate through array to find the villager that matches input
             allVillagers.forEach(function(villager){
                 villName = Object.values(villager.name)[0];
                 if(villName === inputVill.value){
+                    //If there is a match, add the villager to the search results
                     addVillager(villager, false)
                 }
             })
 
+            //Reset form
+            villForm.reset();
         })
 
-        //villForm.reset();
     })
 
 
 });
 
+//Updates "Now Playing:" title, album cover, and audio player
+function changeSong(name, picture, audio) {
+    let player = document.querySelector(".musicPlayer");
+
+    //Title
+    player.childNodes[1].innerHTML = "Now Playing: " + name
+    //Album cover
+    player.childNodes[3].src = picture
+    player.childNodes[3].alt = "K.K. Slider's "+ name +" Album Cover"
+    //Audio player
+    player.childNodes[5].src = audio
+    player.childNodes[5].childNodes[1].href = audio
+}
+
+//Adds villager cards to search results
+function addVillager(villager, island=true){   
+    console.log(villager)
+    const newVill = document.createElement("div");
+    newVill.classList.add("card");
+    
+    //Name
+    const villName = document.createElement("h2");
+    villName.innerHTML = Object.values(villager.name)[0];
+
+    //Icon - shows when user wants to remove villager from island
+    const villIcon = document.createElement("img");
+    villIcon.src = villager.icon_uri;
+    villIcon.alt = "Icon of "+ villName.innerHTML;
+    villIcon.classList.add("villager-icon")
+    villIcon.hidden = true;
+
+    //Picture
+    const villPic = document.createElement("img");
+    villPic.src = villager.image_uri;
+    villPic.alt = "Picture of "+ villName.innerHTML;
+    villPic.classList.add("villager-pic")
+
+    //Species and personality
+    const villSpePers = document.createElement("h4");
+    villSpePers.innerHTML = villager.personality + " " + villager.species
+    
+    //Button to add villager to island
+    const villAdd = document.createElement("button");
+    villAdd.id = "search_"+villName.innerHTML
+    villAdd.innerHTML = "Add to island"
+
+    //Add to DOM
+    newVill.append(villName, villIcon, villPic, villSpePers, villAdd)
+    document.querySelector("#vill-results").append(newVill);
+
+    //Add event listeners to buttons
+    villBtns(villAdd, newVill, false)
+}
+
+//Handles all villager button events
+function villBtns(button, villElement, island=true, wanted=true){
+    //Variable island is true when the villager is in user's island. 
+    //      It is false when the villager is in the search results.
+    //Variable wanted is true when the villager is in the search results or when it is first added to the island.
+    //      It is false when the user clicks planning to remove, so they do not want the villager.
+    button.addEventListener("click", ()=>{
+        if(island === true){
+            //Villager is in the user's island
+            if(wanted === true){
+                //Villager was wanted, but we are changing to unwanted status (showing icon)
+                villElement.querySelector(".villager-pic").hidden = true;
+                villElement.querySelector(".villager-icon").hidden = false;
+                villElement.querySelector("button").innerHTML = "Nevermind, I want them"
+                wanted = false
+            } else{
+                //Villager was unwanted, but we are changing to wanted status (showing portrait)
+                villElement.querySelector(".villager-pic").hidden = false;
+                villElement.querySelector(".villager-icon").hidden = true;
+                villElement.querySelector("button").innerHTML = "Planning to remove"
+                wanted = true
+            }
+            
+        } else{
+            //Villager is in the search results
+            //Clicking will move villager to the user's island with wanted status
+            villElement.querySelector("button").innerHTML = "Planning to remove"
+            document.querySelector("#current-island").append(villElement);
+            island = true
+        }
+    })
+}
 
 
